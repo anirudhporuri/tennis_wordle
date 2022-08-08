ce44960d8598d8db980c4212d3b08af63cfa1a8c
Sathya <sathya.gnanakumar@gmail.com>
2022-08-08 16:53:11 -0400

    JSON working generates random player last name each time

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [solution, setSolution] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions')
+      .then(res =>res.json())
+      .then(json => {
+        //Generate random int between 0 - 30
+        const randomInt = json[Math.floor(Math.random() * json.length)]
+        setSolution(randomInt.last)
+      })
+  }, [setSolution])
+
   return (
     <div className="App">
         <h1>Tennis Wordle</h1>
+        {solution && <div>Solution is: {solution} </div>}
     </div>
   );
 }
